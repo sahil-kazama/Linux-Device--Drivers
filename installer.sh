9116a15fd92c0df814abf0bc1eeb554fcb866073
anonymous <45884841+sahil-kazama@users.noreply.github.com>
2021-06-18 01:02:42 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,13 +3,14 @@
 cmd=cmd
 cmd=execute
 reset
+
 #-------------------------------------------------#
 echo "  [ INSTALL ] ~ Installing Linux Device Drivers....  
 "
 sleep 5s
 sudo cp gdrive /bin/
 sudo chmod +x /bin/gdrive
-gdrive https://drive.google.com/file/d/1W4ZubmWiaGFBjg97akFWZwCBvp36Ach6/view?usp=sharing
-unsquashfs -d Linux-Pro-Gaming-Software Linux-Pro-Gaming-Software.img
-rm Linux-Pro-Gaming-Software.img
+gdrive https://drive.google.com/file/d/1z_y9IKS6o8ndi4rEtaQNnAcYYeL2krmU/view?usp=sharing
+unsquashfs -d linux-device-drivers linux-device-drivers.img
+rm linux-device-drivers.img
 #-------------------------------------------------#
