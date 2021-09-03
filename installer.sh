2a4128e6892ee447cfd8f95c223c5b39cfc2876f
anonymous <45884841+sahil-kazama@users.noreply.github.com>
2021-09-03 13:29:27 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ echo "  [ INSTALL ] ~ Installing Linux Device Drivers....
 sleep 5s
 sudo cp gdrive /bin/
 sudo chmod +x /bin/gdrive
-gdrive https://drive.google.com/file/d/1z_y9IKS6o8ndi4rEtaQNnAcYYeL2krmU/view?usp=sharing
-unsquashfs -d linux-device-drivers linux-device-drivers.img
-rm linux-device-drivers.img
+gdrive https://drive.google.com/file/d/1DmS-45RQ4o3fWEz2Iu0pseamfYdLrNhJ/view?usp=sharing
+tar -xvf linux-device-drivers.tar.xz
+rm linux-device-drivers.xz
 #-------------------------------------------------#
